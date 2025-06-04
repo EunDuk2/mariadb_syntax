0cdaca6a0fc0e51faccbb51a48509a8850b4cd11
Eunduk2 <dmstjd7547@gami.com>
2025-06-04 15:19:22 +0900

    fix

--- GIT_PRACTICE/2_basic_command.sh
+++ GIT_PRACTICE/2_basic_command.sh
@@ -47,8 +47,14 @@ git reset head^
 # 이미 push된 commit사항 되돌리는(완전한 취소X) commitID생성
 git revert 커밋ID
 
-
-
+# 작업중인사항을 임시 저장
+git stash
+# 마지막으로 저장한 사항을 꺼내서 적용
+git stash pop
+# 저장한 작업목록 조회
+git stash list
+# 저장한 목록 전체 삭제
+git stash clear
 
 
 
